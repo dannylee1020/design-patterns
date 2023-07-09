74e42dc9d0f34d958a01bc7e5823e4afeb41fcc4
Daniel Lee <daniel.lee@circleci.com>
2023-07-08 20:40:06 -0700

    adding state pattern

---
+++ state/state.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+type State interface {
+	Login()
+	Logout()
+}
+
+type Context struct {
+	authState bool
+}
+
+type Auth struct{}
+
+func (a *Auth) Login(c *Context) {
+	if c.authState {
+		panic("User is already logged in")
+	}
+	fmt.Println("Logging in...")
+	c.authState = true
+}
+
+func (a *Auth) Logout(c *Context) {
+	if !c.authState {
+		panic("User is not logged in")
+	}
+
+	fmt.Println("Logging out...")
+	c.authState = false
+}
+
+func main() {
+	ctx := Context{authState: false}
+	auth := Auth{}
+
+	auth.Login(&ctx)
+	auth.Logout(&ctx)
+	auth.Logout(&ctx)
+}
